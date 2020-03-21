70281647cdb5f1818983e7d464b6f794f86acb57
barbosamaatheus <matheustdd@gmail.com>
2020-03-21 17:35:41 -0300

    fix: update index.js

--- src/index.js
+++ src/index.js
@@ -2,41 +2,52 @@ const twit = require("twit");
 const config = require("./config.js");
 const T = new twit(config);
 
+const q = "João Pessoa";
+
 var query = {
-  q: ["João Pessoa", "joão pessoa", "JOÃO PESSOA"],
+  q,
   count: 10,
   result_type: "recent"
 };
 
 // This function finds the latest tweet with the #MeetMaye hashtag, and retweets it.
-function retweetLatest() {
+function retweet() {
   T.get("search/tweets", query, function(error, data) {
     // log out any errors and responses
-    console.log(error, data);
-    // If our search request to the server had no errors...
-    if (!error) {
-      // ...then we grab the ID of the tweet we want to retweet...
-      var retweetId = data.statuses[0].id_str;
-      // ...and then we tell Twitter we want to retweet it!
-      T.post("statuses/retweet/" + retweetId, {}, function(error, response) {
-        if (response) {
-          console.log("Success! Retweet Successful!");
-        }
-        // If there was an error with our Twitter call, we print it out here.
-        if (error) {
-          console.log("There was an error with Twitter:", error);
-        }
-      });
-    }
-    // However, if our original search request had an error, we want to print it out here.
-    else {
-      console.log("There was an error with your hashtag search:", error);
-    }
+
+    data.statuses.map(item => {
+      const { id_str, text } = item;
+
+      const result = {
+        id_str,
+        text
+      };
+      console.log(error, result);
+      // If our search request to the server had no errors...
+      if (!error && result.text.includes(q)) {
+        // ...then we grab the ID of the tweet we want to retweet...
+        var retweetId = result.id_str;
+        // ...and then we tell Twitter we want to retweet it!
+        T.post("statuses/retweet/" + retweetId, {}, function(error, response) {
+          if (response) {
+            console.log("Success! Retweet Successful!");
+          }
+          // If there was an error with our Twitter call, we print it out here.
+          if (error) {
+            console.log("There was an error with Twitter:", error);
+          }
+        });
+      }
+      // However, if our original search request had an error, we want to print it out here.
+      else {
+        console.log("There was an error with your hashtag search:", error);
+      }
+    });
   });
 }
 
 // Try to retweet something as soon as we run the program...
-retweetLatest();
+retweet();
 // ...and then every hour/half after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(retweetLatest, 1000 * 60);
+setInterval(retweet, 1000 * 60);
