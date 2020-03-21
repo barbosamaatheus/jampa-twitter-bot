f0c4cd2ead05bdd205d86d46eb484672cd4768bb
barbosamaatheus <matheustdd@gmail.com>
2020-03-21 16:38:36 -0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,41 @@ const twit = require("twit");
 const config = require("./config.js");
 const T = new twit(config);
 
-const stream = T.stream("statuses/filter", {
-  track: ["João Pessoa", "joão pessoa", "JOÃO PESSOA"]
-});
+var query = {
+  q: ["João Pessoa", "joão pessoa", "JOÃO PESSOA"],
+  count: 1,
+  result_type: "recent"
+};
 
-// use this to log errors from requests
-function responseCallback(err, data, response) {
-  console.log(err);
+// This function finds the latest tweet with the #MeetMaye hashtag, and retweets it.
+function retweetLatest() {
+  T.get("search/tweets", query, function(error, data) {
+    // log out any errors and responses
+    console.log(error);
+    // If our search request to the server had no errors...
+    if (!error) {
+      // ...then we grab the ID of the tweet we want to retweet...
+      var retweetId = data.statuses[0].id_str;
+      // ...and then we tell Twitter we want to retweet it!
+      T.post("statuses/retweet/" + retweetId, {}, function(error, response) {
+        if (response) {
+          console.log("Success! Retweet Successful!");
+        }
+        // If there was an error with our Twitter call, we print it out here.
+        if (error) {
+          console.log("There was an error with Twitter:", error);
+        }
+      });
+    }
+    // However, if our original search request had an error, we want to print it out here.
+    else {
+      console.log("There was an error with your hashtag search:", error);
+    }
+  });
 }
 
-// event handler
-stream.on("tweet", tweet => {
-  // retweet
-  T.post("statuses/retweet/:id", { id: tweet.id_str }, responseCallback);
-});
+// Try to retweet something as soon as we run the program...
+retweetLatest();
+// ...and then every hour/half after that. Time here is in milliseconds, so
+// 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
+setInterval(retweetLatest, 1000 * 60);
