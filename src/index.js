0c183e04e127c74b03a9803ff9deca1d80448a44
Barbosa <mbo2@cin.ufpe.br>
2020-08-26 17:45:59 -0300

    update index

--- src/index.js
+++ src/index.js
@@ -54,5 +54,3 @@ retweet();
 // ...and then every hour/half after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
 setInterval(retweet, 1000 * 60 * 5);
-
-app.listen(PORT);
